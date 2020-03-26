0bf71686a42cddabca36b4f9fdfb1e42587ffcb3
zuo785843091 <785843091@qq.com>
2020-03-26 10:30:26 +0800

    scratchpad patch

--- config.h
+++ config.h
@@ -67,6 +67,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 
 static Key keys[] = {
